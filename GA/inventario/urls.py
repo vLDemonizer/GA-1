5aca023e050e19466d8c32521151941c2219c54f
Guillermo Lunar <maxinsideyou@gmail.com>
2017-09-03 11:44:27 -0700

    Fixed namespace errors added base report app

--- GA/inventario/urls.py
+++ GA/inventario/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from .views import (
     UserCreate, MoveInCreate, MoveOutView, ProductClassCreate,
-    LandingPage, UserCreate, Login, log_out, get_product_global_stock,
-    get_product_class_details, get_products, get_product_stock,
+    ProductClassList, LandingPage, UserCreate, Login, log_out,
+    get_product_global_stock, get_product_class_details,
+    get_products, get_product_stock,
 )
 
 app_name = 'inventario'
@@ -12,6 +13,8 @@ urlpatterns = [
     url(r'^create-user/$', UserCreate.as_view(), name='user-create'),
     url(r'^home/$', LandingPage.as_view(), name='home'),
     url(r'^product-create/$', ProductClassCreate.as_view(), name='create-product'),
+    url(r'^product-search/$', ProductClassList.as_view(), name='product-class-search'),
+    #url(r'^product-edit/(?P<pk>[0-9]+)/$', ProductClassEdit.as_view(), name='product-class-edit'),
     url(r'^product-out/$', MoveOutView.as_view(), name='move-out'),
     url(r'^product-in/$', MoveInCreate.as_view(), name='move-in'),
     url(r'^ajax/product_class_details/$', get_product_class_details, name='get_product_class'),
