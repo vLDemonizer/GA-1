99d1efd457b2e78da31fab744803fc858321ea08
Servando Bermudez <servando@banzai.io>
2018-10-29 15:58:46 -0400

    try

--- GA/rrhh/admin.py
+++ GA/rrhh/admin.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+"""
 from .models import Employee, EmployeeControl, Product
 
-"""
 admin.site.register(Employee)
 admin.site.register(Product)
 admin.site.register(EmployeeControl)"""
