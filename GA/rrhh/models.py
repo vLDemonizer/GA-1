f9cdb93e0bc260624275431c9fcc9d5d782bebb4
Servando Bermudez <servando@banzai.io>
2018-10-29 16:25:29 -0400

    try

--- GA/rrhh/models.py
+++ GA/rrhh/models.py
@@ -49,58 +49,6 @@ class Employee(models.Model):
             self.name + ' ' + self.last_name
         )
 
-
-class Spouse(models.Model):
-    employee = models.OneToOneField(Employee, on_delete=models.CASCADE, blank=True, null=True)
-    name = models.CharField(max_length=128) # First and Second Name
-    last_name = models.CharField(max_length=128) # First and Second Last Name
-    cedula = models.IntegerField(unique=True, blank=True, null=True)
-    gender = models.CharField(max_length=20, choices=GENDER_CHOICES, default='F')
-    birthdate = models.DateField()
-    mobile_phone = models.CharField(max_length=40)
-
-    def __str__(self):
-        return str(
-            str(self.employee.employee_id) + ' ' + self.name + ' ' + self.last_name
-        )
-
-
-class Spawn(models.Model):
-    """
-    Childs of the employee
-    """
-    employee = models.ForeignKey(Employee, on_delete=None, blank=True, null=True)
-    name = models.CharField(max_length=128) # First and Second Name
-    last_name = models.CharField(max_length=128) # First and Second Last Name
-    gender = models.CharField(max_length=20, choices=GENDER_CHOICES, default='F')
-    birthdate = models.DateField()
-
-    def __str__(self):
-        return str(
-            str(self.employee.employee_id) + ' ' + self.name + ' ' + self.last_name
-        )
-
-
-class Position(models.Model):
-    """
-    Description of the shift, salary and position in the enterprise
-    """
-    employee = models.OneToOneField(Employee, on_delete=models.CASCADE, blank=True, null=True)
-    name = models.CharField(max_length=128) # Position Full Name
-    base_salary = models.FloatField(default=0)
-    food_salary = models.FloatField(default=0)
-    start = models.DateField() # Position Start Date
-    end = models.DateField() # Position End Date
-    is_active = models.BooleanField(default=True)
-    pay_range = models.CharField(max_length=20, choices=PAY_RANGE_CHOICES, default='S')
-    shift = models.CharField(max_length=20, choices=SHIFT_CHOICES, default='D')
-
-    def __str__(self):
-        return str(
-            str(self.employee.employee_id) + ' ' + self.employee.name + ' ' + self.name + ' ' + str(self.salary)
-        )
-
-
 class Product(models.Model):
     name = models.CharField(max_length=250, default='')
     type = models.CharField(max_length=250, help_text="Talla, Color o Descripcion", default='')
